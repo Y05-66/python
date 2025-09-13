1124e01e7a71493c2dfeb89e6f1bfe2bff9f9975
Y05-66 <2889492383qq.com>
2025-09-13 09:47:13 +0800

    test1

--- Day4/test1.py
+++ Day4/test1.py
@@ -9,3 +9,19 @@ print(type(my_list))
 my_list = [[1,2,3],[4,5,6]]
 print(my_list)
 print(type(my_list))
+
+# 练习
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Bob']
+]
+
+# 打印Apple
+print(L[0][0])
+
+# 打印Python
+print(L[1][1])
+
+# 打印Bob
+print(L[2][2])
